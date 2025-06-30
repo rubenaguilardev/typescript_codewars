6fb101c3ebaa4b2f4afa7d523a6ea037d7af96dd
rubenaguilardev <ragui56@wgu.edu>
2025-06-30 13:20:26 -0700

    organize

--- 8_kyu-Even_or_Odd.ts
+++
@@ -1,9 +0,0 @@
-/* Create a function that takes an integer as an argument and returns 
-"Even" for even numbers or "Odd" for odd numbers. */
-
-export const evenOrOdd = (n: number): string => (
-    n % 2 === 0 ? 'Even' : 'Odd'
-)
-
-
-console.log(evenOrOdd(5))
