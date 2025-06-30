f8abfdcb02bcae8da55fa6cb10a199cfe02c4469
rubenaguilardev <ragui56@wgu.edu>
2025-06-30 02:59:54 -0700

    reversed strings solution

---
+++ 9_kyu_Reversed_Strings.ts
@@ -0,0 +1,6 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+export const solution = (str: string): string => str.split('').reverse().join('')
