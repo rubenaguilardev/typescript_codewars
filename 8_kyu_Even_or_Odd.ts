4e6e0491c4c416d3206a315659c535835f204990
rubenaguilardev <ragui56@wgu.edu>
2025-06-30 02:39:19 -0700

    even of odd solution

---
+++ 8_kyu_Even_or_Odd.ts
@@ -0,0 +1,4 @@
+// Create a function that takes an integer as an argument and returns
+//  "Even" for even numbers or "Odd" for odd numbers.
+
+export const evenOrOdd = (n: number): string => n % 2 === 0 ? 'Even' : 'Odd'
