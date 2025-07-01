45eb40627f358ef3236e596a103c8362c3cd6c37
rubenaguilardev <ragui56@wgu.edu>
2025-07-01 03:59:20 -0700

    century from year solution

---
+++ 8-kyu/Century_From_Year.ts
@@ -0,0 +1,12 @@
+/* Introduction
+The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+Task
+Given a year, return the century it is in. */
+
+
+export const centuryFromYear = (year: number): number => {
+    return Math.ceil(year / 100)
+}
+
+console.log(centuryFromYear(2742))
