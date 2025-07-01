c6a095541e9fe6db83afd11f59cfae94867c13fe
rubenaguilardev <ragui56@wgu.edu>
2025-07-01 16:07:37 -0700

    returning strings solution

---
+++ 8-kyu/Returning_Strings.ts
@@ -0,0 +1,5 @@
+// Create a function that accepts a parameter representing a name and returns the 
+// message: "Hello, <name> how are you doing today?".
+
+
+export const greet = (name: string): string => `Hello, ${name} how are you doing today?`
