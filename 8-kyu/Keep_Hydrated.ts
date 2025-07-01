1b895a94167e9c2f09177d7752044cd8d2070bd3
rubenaguilardev <ragui56@wgu.edu>
2025-06-30 23:24:31 -0700

    keep hydrated solution

---
+++ 8-kyu/Keep_Hydrated.ts
@@ -0,0 +1,11 @@
+/* Nathan loves cycling.
+
+Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+You get given the time in hours and you need to return the number of litres Nathan will drink, rounded down. */
+
+
+export const litres = (time: number): number => Math.floor(time * .5)
+
+console.log(litres(12.3))
+
